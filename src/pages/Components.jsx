da1d24eb888603266a083a46583c35cf2d2ca7fa
Athar Laique <atharlaique15@gmail.com>
2020-06-03 17:04:28 +0500

    Update Components.jsx

--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Components() {
     return (
-        <div>
-               <h3 className="text-center">Component</h3>
-        </div>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <ul style={{listStyle:'none'}}>
+                    <li>
+                       <h6>Button</h6> 
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-8"></div>
+    </div>
     )
 }
